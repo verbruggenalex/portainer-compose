8fedbb68a2d97dc230d518521e8dd7391fcbcc79
Alex Verbruggen <verbruggenalex@gmail.com>
2018-01-15 13:40:20 -0800

    T

--- templates/stacks/subsite-install.sh
+++ templates/stacks/subsite-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ ! -d .git ];then
-  git init
-  git remote add origin ${PROJECT_REPOSITORY}
-fi
-git pull origin master
+env
 
+if [ ! -d .git ];then
+  git clone ${PROJECT_REPOSITORY}
+fi
+sleep 60
 composer install
 
 if [ -L toolkit ]; then
-  ./vendor/ec-europa/toolkit/bin/phing build-platform build-subsite-dev install-clean
+  ./vendor/ec-europa/toolkit/bin/phing build-platform build-subsite-dev install-clean -logger phing.listener.AnsiColorLogger
 fi
